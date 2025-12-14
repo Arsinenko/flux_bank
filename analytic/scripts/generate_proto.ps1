5773b81372a75282a495a52b61830259e8a6fdfd
Arsinenko <kimp3061@gmail.com>
2025-12-14 16:25:19 +0300

    feat: add PowerShell script to generate gRPC Python code from proto files

--- analytic/scripts/generate_proto.ps1
+++ analytic/scripts/generate_proto.ps1
@@ -7,6 +7,7 @@ uv run -m grpc_tools.protoc `
     -I $ProtoDir `
     --python_out=$OutDir `
     --grpc_python_out=$OutDir `
+    --mypy_out=$OutDir `
     "$ProtoDir/*.proto"
 
 Write-Host "Done."
