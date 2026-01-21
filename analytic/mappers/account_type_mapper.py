896bc19ea9b976fa72aed7b0624d9a75b05f48c4
Arsinenko <kimp3061@gmail.com>
2026-01-21 11:40:35 +0300

    customer analytic service

---
+++ analytic/mappers/account_type_mapper.py
@@ -0,0 +1,29 @@
+﻿from typing import List
+
+from api.generated.account_type_pb2 import AccountTypeModel
+from domain.account.account_type import AccountType
+
+
+class AccountTypeMapper:
+    @staticmethod
+    def to_domain(model: AccountTypeModel) -> AccountType:
+        return AccountType(
+            type_id=model.type_id,
+            name=model.name,
+            description=model.description
+        )
+    @staticmethod
+    def to_model(domain: AccountType) -> AccountTypeModel:
+        return AccountTypeModel(
+            type_id=domain.type_id,
+            name=domain.name,
+            description=domain.description
+        )
+
+    @staticmethod
+    def to_domain_list(models: List[AccountTypeModel]) -> List[AccountType]:
+        return [AccountTypeMapper.to_domain(model) for model in models]
+
+    @staticmethod
+    def to_model_list(domains: List[AccountType]) -> List[AccountTypeModel]:
+        return [AccountTypeMapper.to_model(domain) for domain in domains]
---
+++ analytic/services/account_type_analytic.py
@@ -0,0 +1,27 @@
+﻿from api.generated.account_type_analytic_pb2_grpc import AccountTypeAnalyticServiceServicer
+from api.generated.custom_types_pb2 import CountResponse, GetAllRequest
+from domain.account.account_type_repo import AccountTypeRepositoryAbc
+from mappers.account_type_mapper import AccountTypeMapper
+
+
+class AccountTypeAnalytic(AccountTypeAnalyticServiceServicer):
+    def __init__(self, account_type_repo: AccountTypeRepositoryAbc):
+        self.account_type_repo = account_type_repo
+
+    async def ProcessGetCount(self, request, context):
+        result = await self.account_type_repo.get_count()
+        return CountResponse(count=result)
+    async def ProcessGetByIds(self, request, context):
+        result = await self.account_type_repo.get_by_ids(request.ids)
+        return AccountTypeMapper.to_model_list(result)
+
+    async def ProcessGetById(self, request, context):
+        result = await self.account_type_repo.get_by_id(request.id)
+        return AccountTypeMapper.to_model(result) if result else None
+
+    async def ProcessGetAll(self, request: GetAllRequest, context):
+        result = await self.account_type_repo.get_all(page_n=request.pageN,
+                                                      page_size=request.pageSize,
+                                                      order_by=request.order_by,
+                                                      is_desc=request.is_desc)
+        return AccountTypeMapper.to_model_list(result)
